ba23382a9478ba6aebfaa123997f893466efc864
Bullzee <christopher.bull@uoit.net>
2020-02-01 01:02:40 -0500

    MagnetBoots and Player movement

---
+++ GGJ2020_Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementSpeed = 10f;
+    }
+
+    void Update()
+    {
+        playerMove();
+    }
+
+    public void playerMove()
+    {
+        //limit angles of camera to 0 so player doesn't try to move up or down
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * movementSpeed * Time.deltaTime);
+    }
+
+
+
+}
