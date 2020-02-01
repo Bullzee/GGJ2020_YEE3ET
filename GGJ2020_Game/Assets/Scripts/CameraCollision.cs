afa172dd709dc8360e4f31ee6592a2bb45930864
Bullzee <christopher.bull@uoit.net>
2020-02-01 18:30:01 -0500

    [ADD] LWRP, Camera Collision

---
+++ GGJ2020_Game/Assets/Scripts/CameraCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 normalPosition;
+    [SerializeField]
+    float rayDistance, groundOffset, transistionTime;
+
+    public Transform rayTransform;
+
+    RaycastHit cameraHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        normalPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(rayTransform.position, -rayTransform.forward, out cameraHit, rayDistance))
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(cameraHit.point.x, cameraHit.point.y + groundOffset, cameraHit.point.z), transistionTime * Time.deltaTime);
+        }
+        else
+            transform.localPosition = Vector3.Lerp(transform.localPosition, normalPosition, transistionTime * Time.deltaTime);
+    }
+}
