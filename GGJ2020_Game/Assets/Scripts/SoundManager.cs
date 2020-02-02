69c14aa11efdf4d3b27a513c4b8e1112d26a85fd
AtiyaNova <atiyanova@gmail.com>
2020-02-02 13:00:23 -0500

    [ADD] Sounds, Skybox
    Added Bill's sounds
    Started the skybox
    Started rough sound manager

---
+++ GGJ2020_Game/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource bgMusic, jumpSound, magnetSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgMusic.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
